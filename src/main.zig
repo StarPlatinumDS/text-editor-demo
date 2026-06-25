5e33cde63b8d4757f18684ac9af47ea30cc91196
hehehehheh <you@example.com>
2026-06-25 22:52:09 +0300

    update main and build

--- src/main.zig
+++ src/main.zig
@@ -2,12 +2,14 @@ const std = @import("std");
 // add posix def
 const posix = std.posix;
 
+const terminal = @import("terminal.zig");
+
 pub fn main() !void {
     // probably a buffer to hold keypress
     var buf: [1]u8 = undefined;
 
     while (true) {
-        const n = try posix.read(0, buf[0..]);
+        const n = try posix.read(terminal.stdin_fd, buf[0..]);
 
         if (n == 0) break;
         if (buf[0] == 'q') break;

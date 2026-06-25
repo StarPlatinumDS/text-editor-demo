6b389cd0e2d383ca6b03cc53dbc482078935df0f
hehehehheh <you@example.com>
2026-06-25 22:07:14 +0300

    read user keypress

---
+++ src/main.zig
@@ -0,0 +1,15 @@
+const std = @import("std");
+// add posix def
+const posix = std.posix;
+
+pub fn main() !void {
+    // probably a buffer to hold keypress
+    var buf: [1]u8 = undefined;
+
+    while (true) {
+        const n = try posix.read(0, buf[0..]);
+
+        if (n == 0) break;
+        if (buf[0] == 'q') break;
+    }
+}

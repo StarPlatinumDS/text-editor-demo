8a7baa804159097f0f5e92179fd15a26796e7b10
hehehehheh <you@example.com>
2026-06-26 21:08:43 +0300

    add welcome message

--- src/editor.zig
+++ src/editor.zig
@@ -1,6 +1,5 @@
 const std = @import("std");
 const terminal = @import("terminal.zig");
-const posix = std.posix;
 
 // ....
 fn ctrlKey(comptime c: u8) u8 {
@@ -35,7 +34,7 @@ pub fn refreshScreen(init: std.process.Init) !void {
     // Move cursor to the top-left
     try append_buf.appendSlice(init.gpa, "\x1b[H");
 
-    try drawRows(init.gpa, &append_buf, screen_size.rows);
+    try drawRows(init.gpa, &append_buf, screen_size);
 
     // Move cursor back to top-left
     try append_buf.appendSlice(init.gpa, "\x1b[H");
@@ -48,18 +47,40 @@ pub fn refreshScreen(init: std.process.Init) !void {
 }
 
 // ....
-fn drawRows(allocator: std.mem.Allocator, append_buffer: *std.ArrayList(u8), rows: usize) !void {
+fn drawRows(allocator: std.mem.Allocator, append_buffer: *std.ArrayList(u8), screen_size: terminal.WindowSize) !void {
     var y: usize = 0;
-    while (y < rows) : (y += 1) {
-        try append_buffer.appendSlice(allocator, "~");
+    while (y < screen_size.rows) : (y += 1) {
+        if (y == screen_size.rows / 3) {
+            try drawWelcome(allocator, append_buffer, screen_size.cols);
+        } else {
+            try append_buffer.appendSlice(allocator, "~");
+        }
 
         // clear the restof the line
         try append_buffer.appendSlice(allocator, "\x1b[K");
 
         // this is needed so that terminal doesn't scroll
         // by one line after end
-        if (y + 1 < rows) {
+        if (y + 1 < screen_size.rows) {
             try append_buffer.appendSlice(allocator, "\r\n");
         }
     }
+}
+
+fn drawWelcome(allocator: std.mem.Allocator, append_buffer: *std.ArrayList(u8), cols: usize) !void {
+    const welcome = "Text editor -- version 0.0.1";
+
+    const welcome_len = @min(welcome.len, cols);
+    var padding = (cols - welcome_len) / 2;
+
+    if (padding > 0) {
+        try append_buffer.appendSlice(allocator, "~");
+        padding -= 1;
+    }
+
+    while (padding > 0) : (padding -= 1) {
+        try append_buffer.appendSlice(allocator, " ");
+    }
+
+    try append_buffer.appendSlice(allocator, welcome[0..welcome_len]);
 }
